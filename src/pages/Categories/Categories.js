3112cfedec6def89acb01e5961c2cfba86e1b45d
bbogum19 <berkaye2001@hotmail.com>
2023-01-31 15:07:23 +0300

    Data fetched from REST API displayed on the screen.

--- src/pages/Categories/Categories.js
+++ src/pages/Categories/Categories.js
@@ -1,12 +1,32 @@
-import React from "react";
-import { SafeAreaView, Text } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { View, Text, FlatList } from 'react-native';
 import styles from './Categories.style';
+import axios from "axios";
+import CategoryCard from '../../components/CategoryCard';
 
 const Categories = () => {
+
+    const [data, setData] = useState([]);
+
+    const fetchData = async () => {
+        const url = "https://www.themealdb.com/api/json/v1/1/list.php?c=list";
+        const { data: responseData } = await axios.get(url);
+        setData(responseData.meals)
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+
+    const renderCategory = ({ item }) => <CategoryCard category={item} />
+
     return (
-        <SafeAreaView>
-            <Text> Categories Page! </Text>
-        </SafeAreaView>
+        <View>
+            <FlatList
+                renderItem={renderCategory}
+                data={data}
+            />
+        </View>
     )
 }
 

80fa0adb1db92777fc74499208b0dbb1cd298817
bbogum19 <berkaye2001@hotmail.com>
2023-02-01 10:05:12 +0300

    Created useFetch custom hook.

---
+++ src/hooks/useFetch/useFetch.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+import axios from "axios";
+
+const useFetch = (url) => {
+
+    const [data, setData] = useState([]);
+
+    const fetchData = async () => {
+        const { data: responseData } = await axios.get(url);
+        setData(responseData.meals)
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+
+    return data;
+}
+
+export default useFetch;
--- src/pages/Categories/Categories.js
+++ src/pages/Categories/Categories.js
@@ -1,23 +1,15 @@
-import React, { useState, useEffect } from "react";
-import { View, Text, FlatList } from 'react-native';
+import React from "react";
+import { View, FlatList } from 'react-native';
 import styles from './Categories.style';
-import axios from "axios";
 import CategoryCard from '../../components/CategoryCard';
 import { API_KEY } from '@env';
+import useFetch from '../../hooks/useFetch/useFetch';
 
 const Categories = () => {
 
-    const [data, setData] = useState([]);
+    const category_url = API_KEY + "list.php?c=list";
 
-    const fetchData = async () => {
-        const url = `${API_KEY}list.php?c=list`;
-        const { data: responseData } = await axios.get(url);
-        setData(responseData.meals)
-    }
-
-    useEffect(() => {
-        fetchData();
-    }, [])
+    const data = useFetch(category_url)
 
     const renderCategory = ({ item }) => <CategoryCard category={item} />
 

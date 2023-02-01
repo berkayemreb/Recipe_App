1424134f4e9b674b5341cbf6a77fca96a5835b6d
bbogum19 <berkaye2001@hotmail.com>
2023-02-02 02:30:32 +0300

    The styling of the detail screen has been done.

--- src/components/DetailCard/DetailCard.js
+++ src/components/DetailCard/DetailCard.js
@@ -1,11 +1,21 @@
 import React from "react";
-import { View, Text } from 'react-native';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
 import styles from './DetailCard.style';
 
-const DetailCard = ({ meal }) => {
+const DetailCard = ({ meal, goToYoutube }) => {
     return (
-        <View>
-            <Text>{meal.strMeal}</Text>
+        <View style={styles.container}>
+            <Image style={styles.image} source={{ uri: meal.strMealThumb }} />
+            <View style={styles.header_contanier}>
+                <Text style={styles.headerName}>{meal.strMeal}</Text>
+                <Text style={styles.areaName}>-{meal.strArea}</Text>
+            </View>
+            <View style={styles.body_container}>
+                <Text style={styles.instruction_description}>{meal.strInstructions}</Text>
+                <TouchableOpacity style={styles.youtubeLink_container} onPress={() => goToYoutube(meal.strYoutube)}>
+                    <Text style={styles.youtubeLink_text}>Watch on Youtube</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }

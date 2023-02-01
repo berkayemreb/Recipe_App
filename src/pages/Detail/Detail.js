c64f45e0b5afce97cc80c44235dd834d8aaf1308
bbogum19 <berkaye2001@hotmail.com>
2023-02-02 02:33:31 +0300

    The transition to the YouTube URL was completed using linking.

--- src/pages/Detail/Detail.js
+++ src/pages/Detail/Detail.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FlatList, SafeAreaView } from 'react-native';
+import { FlatList, SafeAreaView, Linking } from 'react-native';
 import styles from './Detail.style';
 import useFetch from '../../hooks/useFetch';
 import { API_KEY } from '@env';
@@ -10,10 +10,12 @@ const Detail = ({ route }) => {
 
     const data = useFetch(API_KEY + "lookup.php?i=" + id);
 
-    const renderDetail = ({ item }) => <DetailCard meal={item} />
+    const goToYoutube = url => Linking.openURL(url);
+
+    const renderDetail = ({ item }) => <DetailCard meal={item} goToYoutube={goToYoutube} />
 
     return (
-        <SafeAreaView>
+        <SafeAreaView style={styles.container}>
             <FlatList
                 data={data.meals}
                 renderItem={renderDetail}

cf25138393bbafd82b4ca2468dcbf9d51bb235ec
bbogum19 <berkaye2001@hotmail.com>
2023-02-02 10:59:00 +0300

    The component and FlatList related to the detail page have been removed. The page has been updated.

--- src/pages/Detail/Detail.js
+++ src/pages/Detail/Detail.js
@@ -1,25 +1,44 @@
 import React from "react";
-import { FlatList, SafeAreaView, Linking } from 'react-native';
+import { SafeAreaView, Linking, TouchableOpacity, Image, View, Text, ScrollView } from 'react-native';
 import styles from './Detail.style';
 import useFetch from '../../hooks/useFetch';
 import { API_KEY } from '@env';
-import DetailCard from '../../components/DetailCard';
+import LoadingScreen from "../../components/LoadingScreen";
+import ErrorScreen from "../../components/ErrorScreen";
 
 const Detail = ({ route }) => {
     const { id } = route.params;
 
-    const data = useFetch(API_KEY + "lookup.php?i=" + id);
+    const { data, loading, error } = useFetch(API_KEY + "lookup.php?i=" + id);
+
+    if (loading) {
+        return <LoadingScreen />
+    }
+
+    if (error) {
+        return <ErrorScreen />
+    }
 
     const goToYoutube = url => Linking.openURL(url);
 
-    const renderDetail = ({ item }) => <DetailCard meal={item} goToYoutube={goToYoutube} />
 
     return (
         <SafeAreaView style={styles.container}>
-            <FlatList
-                data={data.meals}
-                renderItem={renderDetail}
-            />
+            <ScrollView>
+                <Image style={styles.image} source={{ uri: data.meals[0].strMealThumb }} />
+                <View style={styles.inner_container}>
+                    <View style={styles.header_contanier}>
+                        <Text style={styles.headerName}>{data.meals[0].strMeal}</Text>
+                        <Text style={styles.areaName}>-{data.meals[0].strArea}</Text>
+                    </View>
+                    <View style={styles.body_container}>
+                        <Text style={styles.instruction_description}>{data.meals[0].strInstructions}</Text>
+                        <TouchableOpacity style={styles.youtubeLink_container} onPress={() => goToYoutube(data.meals[0].strYoutube)}>
+                            <Text style={styles.youtubeLink_text}>Watch on Youtube</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </ScrollView>
         </SafeAreaView>
     )
 }
